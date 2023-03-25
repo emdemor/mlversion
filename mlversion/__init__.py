e7919205e2f623757554f8ddabbb98a58c69cbdb
Eduardo Morais <emdemor415@gmail.com>
2023-03-25 16:09:32 -0300

    🎨 add __all__ do __init__.py

--- mlversion/__init__.py
+++ mlversion/__init__.py
@@ -1,1 +1,9 @@
 from mlversion._version import __version__
+from mlversion._version_handler import ModelVersion, VersionHandler
+from mlversion._artifact import CSVArtifact
+
+__all__ = [
+    "ModelVersion",
+    "VersionHandler",
+    "CSVArtifact",
+]
