8ae83f5df8440b5c46b1c5d01ca9933629ef1646
Eduardo Morais <emdemor415@gmail.com>
2023-03-25 17:10:38 -0300

    🔨 add ExistingVersionError to __all__

--- mlversion/__init__.py
+++ mlversion/__init__.py
@@ -1,9 +1,10 @@
 from mlversion._version import __version__
-from mlversion._version_handler import ModelVersion, VersionHandler
+from mlversion._version_handler import ModelVersion, VersionHandler, ExistingVersionError
 from mlversion._artifact import CSVArtifact
 
 __all__ = [
     "ModelVersion",
     "VersionHandler",
     "CSVArtifact",
+    "ExistingVersionError",
 ]
