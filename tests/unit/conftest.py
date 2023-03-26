13f81f11cf14c978efaafd64b0e632825178c166
Eduardo Morais <emdemor415@gmail.com>
2023-03-26 13:14:47 -0300

    ♻️ refact artifact_handler tests with fixtures

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,9 +1,12 @@
 import shutil
+import pandas as pd
 
 import pytest
 from basix import files
+from sklearn.linear_model import LinearRegression
 
 from mlversion import ModelVersion, VersionHandler
+from mlversion._artifacts import CSVArtifact, BinaryArtifact
 
 
 @pytest.fixture()
@@ -17,6 +20,24 @@ def models_path():
     yield directory
     shutil.rmtree(directory, ignore_errors=True)
 
+@pytest.fixture()
+def csv_artifact():
+    df = pd.DataFrame([[0, 10], [1, 12]], columns=["id", "value"])
+    artifact = CSVArtifact(
+        label="train", content=df, parent_dir="workdir/test/data/"
+    )
+    return artifact
+
+@pytest.fixture()
+def bin_artifact():
+    df = pd.DataFrame([[0, 10], [1, 20]], columns=["x", "y"])
+    estimator = LinearRegression()
+    estimator.fit(df[["x"]], df["y"])
+    artifact = BinaryArtifact(
+        label="linear_regression", content=estimator, parent_dir="workdir/test/models/"
+    )
+    return artifact
+    
 @pytest.fixture()
 def model_version(start_version_string):
     model_version = ModelVersion(start_version_string)
--- tests/unit/test_artifact_handler.py
+++ tests/unit/test_artifact_handler.py
@@ -6,31 +6,24 @@ from mlversion._artifacts import CSVArtifact, BinaryArtifact
 from sklearn.linear_model import LinearRegression
 
 
-def test_csv_artifact():
-    df = pd.DataFrame([[0, 10], [1, 12]], columns=["id", "value"])
-    artifact = CSVArtifact(
-        label="train", content=df, parent_dir="workdir/test/data/"
-    ).save()
+def test_csv_artifact(csv_artifact):
+    csv_artifact.save()
     artifact_imported = CSVArtifact.load(label="train", parent_dir="workdir/test/data/")
+
+    df = csv_artifact.get()
     df_imported = artifact_imported.get()
+
     assert df.equals(df_imported)
 
 
-def test_binary_artifact():
-    expected_value = 30
-
-    df = pd.DataFrame([[0, 10], [1, 20]], columns=["x", "y"])
-    estimator = LinearRegression()
-    estimator.fit(df[["x"]], df["y"])
-    artifact = BinaryArtifact(
-        label="linear_regression", content=estimator, parent_dir="workdir/test/models/"
-    ).save()
+def test_binary_artifact(bin_artifact):
+    expected_value = bin_artifact.get().predict([[2]])[0]
+    bin_artifact.save()
 
     imported_artifact = BinaryArtifact.load(
         label="linear_regression", parent_dir="workdir/test/models/"
     )
-    imported_estimator = imported_artifact.get()
-    prediction = imported_estimator.predict([[2]])[0]
+    prediction = imported_artifact.get().predict([[2]])[0]
 
     assert np.isclose(expected_value, prediction)
 
