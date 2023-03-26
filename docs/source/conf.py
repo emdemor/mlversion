fbe93cf46e512ef83821ddda82c14b58232352a2
Eduardo Morais <emdemor415@gmail.com>
2023-03-26 17:03:32 -0300

    🔖bump version: 0.0.1.dev8 → 0.0.1.dev9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ project = 'MLVersion' # Change app_name
 copyright = '2023, Eduardo Messias de Morais'
 author = 'Eduardo Messias de Morais'
 release = '2023'
-version = "0.0.1.dev8"
+version = "0.0.1.dev9"
 
 
 # -- General configuration ---------------------------------------------------
