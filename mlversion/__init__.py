640f575fccf1d705112a393a0ba35a11f039c297
Eduardo Morais <emdemor415@gmail.com>
2023-03-28 22:49:02 -0300

    🎨 add classes on __all__

--- mlversion/__init__.py
+++ mlversion/__init__.py
@@ -1,10 +1,24 @@
-from mlversion._version import __version__  # noqa: F401
-from mlversion._version_handler import ModelVersion, VersionHandler, ExistingVersionError
+from mlversion._version import __version__
+from mlversion._version_handler import ModelVersion
+from mlversion._version_handler import VersionHandler
 from mlversion._artifacts import CSVArtifact
+from mlversion._artifacts import ParquetArtifact
+from mlversion._artifacts import BinaryArtifact
+from mlversion._artifact_handler import Artifact
+from mlversion._artifact_handler import ArtifactSubGroup
+from mlversion._artifact_handler import ArtifactGroup
+from mlversion._artifact_handler import ArtifactHandler
+
 
 __all__ = [
+    "__version__",
     "ModelVersion",
     "VersionHandler",
     "CSVArtifact",
-    "ExistingVersionError",
+    "ParquetArtifact",
+    "BinaryArtifact",
+    "Artifact",
+    "ArtifactSubGroup",
+    "ArtifactGroup",
+    "ArtifactHandler",
 ]
